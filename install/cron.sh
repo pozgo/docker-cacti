45b69e8c164d5a1bc00779318bbe0c09395b508b
Przemyslaw Ozgo <linux@ozgo.info>
2014-07-30 20:55:32 +0100

    cron.sh Fixed spaces

--- install/cron.sh
+++ install/cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Add crontab job
-echo "*/5 * * * *cacti/usr/bin/php /usr/share/cacti/poller.php > /dev/null 2>&" | crontab -
+echo "*/5 * * * *  cacti  /usr/bin/php /usr/share/cacti/poller.php > /dev/null 2>&" | crontab -
