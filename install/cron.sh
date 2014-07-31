393b54353e1d026451b23c36cb55b8c8aec04f98
Przemyslaw Ozgo <przemek@isc.ac.uk>
2014-07-31 12:11:42 +0100

    Updated cron.sh

--- install/cron.sh
+++ install/cron.sh
@@ -3,8 +3,8 @@
 echo "Starting cron deamon first"
 /usr/sbin/crond
 
-cat "- : root : ALL" >> /etc/security/access.conf 
-  
+echo "root" >> /etc/cron.allow  
+echo "cacti" >> /etc/cron.allow 
 echo "Adding New cronjob for cacti poller"
 echo "*/5    *   *   *   *   /usr/bin/php /usr/share/cacti/poller.php > /dev/null 2>&" | crontab -u cacti -
 
