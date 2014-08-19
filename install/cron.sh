09a9f9a716542ea625c8879cd54de215979ff2fe
Przemyslaw Ozgo <przemek@isc.ac.uk>
2014-08-19 13:48:56 +0100

    Crontab for cacti user fixed to be working.

--- install/cron.sh
+++ install/cron.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "*/5 * * * * cacti /usr/bin/php /usr/share/cacti/poller.php > /dev/null 2>&" > /etc/cron.d/cacti
+crontab /data/config/cron.conf
+sed -i -e 's/^#*//' /etc/cron.d/cacti
 exit 0
